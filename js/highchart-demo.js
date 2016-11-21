245c3dee7dd8d632d53bdb0ce640b962ee8792a9
cheriejw <cheriejawoo@yahoo.com>
2016-11-21 14:04:00 -0800

    Update highchart-demo.js

--- js/highchart-demo.js
+++ js/highchart-demo.js
@@ -190,4 +190,60 @@ $(document).ready(function () {
         }]
 
     });
+    
+    //Another way to graph, adding things from CSV file, using JQuery
+    var options = {
+        chart: {
+            renderTo: 'loadcsv',
+			type: 'column'
+		},
+		title: {
+			text: 'Loaded CSV File'
+		},
+		xAxis: {
+			categories: []
+		},
+		yAxis: {
+			title: {
+			    text: 'Units'
+			}
+		},
+		series: []
+	};
+			
+	//Load the data from the CSV file. This is the contents of the file:
+	$.get('accident.csv', function(data) {
+		// Split the lines
+		var lines = data.split('\n');
+		$.each(lines, function(lineNo, line) {
+			var items = line.split(',');
+			
+			// header line containes categories
+			if (lineNo == 0) {
+				$.each(items, function(itemNo, item) {
+					if (itemNo > 0) options.xAxis.categories.push(item);
+				});
+			}
+			
+			// the rest of the lines contain data with their name in the first position
+			else {
+				var series = { 
+	    			data: []
+				};
+				$.each(items, function(itemNo, item) {
+					if (itemNo == 0) {
+						series.name = item;
+					} else {
+						series.data.push(parseFloat(item));
+					}
+				});
+						
+				options.series.push(series);
+
+			}
+					
+		});
+				
+		var chart = new Highcharts.Chart(options);
+	});
 })
