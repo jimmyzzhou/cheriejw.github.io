a778b0b35639525b6b4d830184345d71cb16f8dd
Cherie Woo <cheriejawoo@yahoo.com>
2016-11-23 12:21:47 -0800

    Added chart.

--- js/highchart-demo.js
+++ js/highchart-demo.js
@@ -253,7 +253,7 @@ $(document).ready(function () {
 	});
 	
 	//Comdination chart. Last chart I will do before I go into how to do dynamic data loading.
-	Highcharts.chart('combination', {
+	Highcharts.chart('combo', {
         title: {
             text: 'Combination chart'
         },
@@ -313,6 +313,70 @@ $(document).ready(function () {
             dataLabels: {
                 enabled: false
             }
+        }]
+    });
+
+    //Spline Highchart
+    Highcharts.chart('spline', {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Monthly Average Temperature'
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature'
+            },
+            labels: {
+                formatter: function () {
+                    return this.value + '°';
+                }
+            }
+        },
+        tooltip: {
+            crosshairs: true,
+            shared: true
+        },
+        plotOptions: {
+            spline: {
+                marker: {
+                    radius: 4,
+                    lineColor: '#666666',
+                    lineWidth: 1
+                }
+            }
+        },
+        series: [{
+            name: 'Tokyo',
+            marker: {
+                symbol: 'square'
+            },
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
+                y: 26.5,
+                marker: {
+                    symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
+                }
+            }, 23.3, 18.3, 13.9, 9.6]
+
+        }, {
+            name: 'London',
+            marker: {
+                symbol: 'diamond'
+            },
+            data: [{
+                y: 3.9,
+                marker: {
+                    symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
+                }
+            }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
         }]
     });
 })
