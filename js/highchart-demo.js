8852b68355a4551e840a3ca1a5ddba949f7757f5
Cherie Woo <cheriejawoo@yahoo.com>
2016-11-23 22:59:27 -0800

    Some minor changes.

--- js/highchart-demo.js
+++ js/highchart-demo.js
@@ -369,24 +369,14 @@ function spline() {$(document).ready(function () {
             marker: {
                 symbol: 'square'
             },
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
-                y: 26.5,
-                marker: {
-                    symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
-                }
-            }, 23.3, 18.3, 13.9, 9.6]
-
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            //{y:#, marker:{symbol:'url(#site)'}} for different.
         }, {
             name: 'London',
             marker: {
                 symbol: 'diamond'
             },
-            data: [{
-                y: 3.9,
-                marker: {
-                    symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
-                }
-            }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
         }]
     });
 })}
