c4d1060b265fc25d2ba3a65652e99e38bec59925
Cherie Woo <cheriejawoo@yahoo.com>
2017-01-04 10:25:16 -0800

    javascript for just one chart, 'no junk'.

--- js/dash-recreate.js
+++ js/dash-recreate.js
@@ -1,1 +1,137 @@
 //All text box are teal and when you click on others turn grey.
+
+// function animation {
+//     $(function () {
+//         var renderer = new Highcharts.Renderer(
+//                 $('#container')[0],
+//                 400, //width
+//                 300 //height
+//             ),
+//             rect = renderer.rect(100, 100, 100, 100, 5)
+//                 .attr({
+//                     'stroke-width': 2,
+//                     stroke: 'gray',
+//                     fill: 'silver',
+//                     zIndex: 3
+//                 })
+//                 .on('click', function () {
+//                     rect.animate({
+//                         x: 50,
+//                         y: 50,
+//                         width: 200,
+//                         height: 20,
+//                         'stroke-width': 10
+//                     });
+//                 })
+//                 .add();
+//     })
+// };
+
+
+// var load_data = function(config, data){ //he said this is their signiature
+// 	console.log(JSON.parse(data)); //something like this...
+// }
+
+//ask him for that js example chart he showed me.
+
+// $(document).ready(function() {
+
+//     var options = {
+//         chart: {
+//             renderTo: 'container',
+//             type: 'spline'
+//         },
+//         series: [{}]
+//     };
+
+
+
+//     $.getJSON('data/csudh_freshman.json', function(data) {
+//         var json_ = JSON.parse("")
+//         console.log(json_.trendData.year)
+        
+//         options.series[0].data = data;
+//         var chart = new Highcharts.Chart(options);
+//     });
+
+// });
+
+ $(function () {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'line' //spline to line?
+        },
+        legend: {
+                title: {
+                    text: "(Click to show/hide items.)",
+                style: {
+                        "fontStyle":"italic",
+                    "fontWeight":"light"
+                }
+            }
+        },
+        title: {
+            text: null
+        },
+        subtitle: {
+            text: null
+        },
+        xAxis: {
+            categories: ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014']
+        },
+        yAxis: {
+                title: {
+                    text: null
+            },
+            labels: {
+                formatter: function () {
+                    return this.value + '%';
+                }
+            }
+        },
+        tooltip: {
+            crosshairs: true,
+            shared: true
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    radius: 4,
+                    lineColor: '#FFFFFF'
+                },
+                color: '#555555'
+            }
+        },
+        series: [{
+            name: '4 Year Grad Major',
+            color: '#8ED8F7',
+            marker: {
+                symbol: 'circle'
+            },
+            data:[null,null,null,null,null,null,null,null,null,null,null,14,null,null,null]}, {
+
+            name: '4 Year Grad Campus',
+            dashStyle: 'Dot',
+            color: '#8ED8F7',
+            marker: {
+                symbol: 'diamond'
+            },
+            data:[6,5,4,5,5,6,4,4,5,6,5,6,null,null,null]}, {
+
+            name: '6 Year Grad Major',
+            marker: {
+                symbol: 'square'
+            },
+            data:[35,38,34,28,35,22,29,31,30,37,28,null,null,null]}, {
+
+            name: '6 Year Grad Campus',
+            dashStyle: 'Dot',
+            marker: {
+                symbol: 'triangle'
+            },
+            data:[33,28,34,35,31,24,28,30,32,35,27,null,null,null]}
+        ]
+    });
+});
+
+
--- js/highchart-demo.js
+++ js/highchart-demo.js
@@ -79,9 +79,28 @@ function bars() {$(document).ready(function () {
         }]
     });
 })}
-
-function bubble() {$(document).ready(function () {
+var mychart = function (data, config) {
+    $(document).ready(function () {
     //Second highchart that is a bubble.
+
+    var mydata = [
+                { x: 95, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
+                { x: 86.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
+                { x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
+                { x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
+                { x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
+                { x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
+                { x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
+                { x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
+                { x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
+                { x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
+                { x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
+                { x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
+                { x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
+                { x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
+                { x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
+            ];
+
     Highcharts.chart(container, {
         chart: {
             type: 'bubble',
@@ -176,23 +195,7 @@ function bubble() {$(document).ready(function () {
 
     // All data plotted on a chart comes from the series object.
         series: [{
-            data: [
-                { x: 95, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
-                { x: 86.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
-                { x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
-                { x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
-                { x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
-                { x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
-                { x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
-                { x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
-                { x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
-                { x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
-                { x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
-                { x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
-                { x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
-                { x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
-                { x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
-            ]
+            data: mydata
         }]
     });
 })}
@@ -258,6 +261,27 @@ function loadcsv() {$(document).ready(function () {
         var chart = new Highcharts.Chart(options);
     });
 })}
+
+// function loadcsv() {$(document).ready(function () {
+//     $.get('data/accident_drunk_hc.csv', function(csv) {
+//         $('#container').highcharts({
+//             chart: {
+//                 type: 'column'
+//             },
+//             data: {
+//                 csv: csv
+//             },
+//             title: {
+//                 text: 'Fruit Consumption'
+//             },
+//             yAxis: {
+//                 title: {
+//                     text: 'Units'
+//                 }
+//             }
+//         });
+//     });
+// })}
 
 function combo() {$(document).ready(function () {
 //Comdination chart. Last chart I will do before I go into how to do dynamic data loading.
